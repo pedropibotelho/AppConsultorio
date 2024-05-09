ea54bae433c693e080d14c31d387861142e6ba46
Pedro Botelho <p2pinheiro2@gmail.com>
2024-05-09 18:27:52 -0300

    login com bd
    realizando o login com banco de dados

--- app/src/main/java/com/example/appconsultorio/FormLogin.java
+++ app/src/main/java/com/example/appconsultorio/FormLogin.java
@@ -14,7 +14,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class FormLogin extends AppCompatActivity {
-
+    private SQLiteDatabase bancoDados;
     private EditText editUser;
     Button btn;
 
@@ -23,7 +23,7 @@ public class FormLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
 
-
+        criarBancoDados();
 
         btn = (Button) findViewById(R.id.butaoLogin);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -34,23 +34,48 @@ public class FormLogin extends AppCompatActivity {
         });
     }
 
+    public void criarBancoDados() {
+        try {
+            bancoDados = openOrCreateDatabase("appconsultorio", MODE_PRIVATE, null);
+            Cursor cursor = bancoDados.rawQuery("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='login'", null);
+            cursor.moveToFirst();
+            int tableCount = cursor.getInt(0);
+            cursor.close();
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS login("+
+                    " id INTEGER PRIMARY KEY AUTOINCREMENT" +
+                    ", user VARCHAR" +
+                    ", senha VARCHAR)");
 
+            bancoDados.execSQL("INSERT INTO login (user, senha) VALUES ('admin','admin')");
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
     public void testeLogin() {
-        String user = "admin";
-        String senha = "admin";
         EditText edtUser = findViewById(R.id.edit_user);
         EditText edtSenha = findViewById(R.id.edit_senha);
         String digitUser = edtUser.getText().toString();
         String digitSenha = edtSenha.getText().toString();
 
-        if((user.equals(digitUser)) && (senha.equals(digitSenha))){
+        SQLiteDatabase bancoDados = openOrCreateDatabase("appconsultorio", MODE_PRIVATE, null);
+
+        // Consulta para verificar se o usuário e senha correspondem
+        Cursor cursor = bancoDados.rawQuery("SELECT COUNT(*) FROM login WHERE user=? AND senha=?", new String[]{digitUser, digitSenha});
+        cursor.moveToFirst();
+        int count = cursor.getInt(0);
+        cursor.close();
+
+        bancoDados.close();
+
+        if (count > 0) {
             Intent i = new Intent(FormLogin.this, TelaPrincipal.class);
             Toast.makeText(this, "Login realizado com sucesso!", Toast.LENGTH_SHORT).show();
             startActivity(i);
-        }else{
+        } else {
             Toast.makeText(this, "Usuário ou senha incorretos.", Toast.LENGTH_SHORT).show();
         }
-
     }
+
 }
